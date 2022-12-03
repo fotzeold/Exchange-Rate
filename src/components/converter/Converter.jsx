f5ae63072f967a648d950a51d4ab8dbbd64b51e8
Roman Popovych <mechenuyzzzz@gmail.com>
2022-12-03 19:45:48 +0200

    add component Converter

---
+++ src/components/converter/Converter.jsx
@@ -0,0 +1,117 @@
+import { useState, useEffect } from 'react';
+import './converter.scss';
+import useSortData from '../../hooks/useSortData';
+
+const Converter = (props) => {
+	const [topValue, setTopValue] = useState('');
+	const [bottomValue, setBottomValue] = useState('');
+
+	const [topLabel, setTopLabel] = useState('UAH');
+	const [bottomLabel, setBottomLabel] = useState('USD');
+
+	const [focusTop, setFocusTop] = useState(false);
+	const [focusBottom, setFocusBottom] = useState(false);
+
+	const [countValueTop, setCountValueTop] = useState('');
+	const [countValueBottom, setCountValueBottom] = useState('');
+
+	const rateList = [{
+		currency: "UAH",
+		id: 100000,
+		name: "Українська гривня",
+		rate: 1
+	}, ...useSortData(props.data)];
+
+	const sense = () => {
+		const objSenseTop = rateList.filter(e => e.currency == topLabel);
+		const objSenseBottom = rateList.filter(e => e.currency == bottomLabel);
+
+		const rateTop = objSenseTop[0].rate;
+		const rateBot = objSenseBottom[0].rate;
+
+		return { rateTop, rateBot }
+	}
+
+	useEffect(() => {
+		onCalc()
+	}, [topValue, bottomValue, topLabel, bottomLabel, countValueTop, countValueBottom])
+
+	const onCalc = () => {
+		const { rateTop, rateBot } = sense();
+
+		if (focusTop) {
+			const resT = topValue * rateTop / rateBot;
+			setCountValueTop(topValue)
+			setCountValueBottom(resT.toFixed(2))
+			setBottomValue(countValueBottom)
+		}
+
+		if (focusBottom) {
+			const resB = bottomValue * rateBot / rateTop;
+			setCountValueBottom(bottomValue)
+			setCountValueTop(resB.toFixed(2))
+			setTopValue(countValueTop)
+		}
+	}
+
+	const onFocusTop = () => {
+		setFocusBottom(false)
+		setFocusTop(true)
+	}
+
+	const onFocusBottom = () => {
+		setFocusTop(false)
+		setFocusBottom(true)
+	}
+
+
+	return (
+		<>
+			<div className="row">
+				<input type="number"
+					onChange={(e) => { setTopValue(e.target.value) }}
+					onFocus={onFocusTop}
+					onBlur={() => setFocusTop(false)}
+					value={focusTop ? topValue : countValueTop}
+				/>
+				<select
+					onChange={(e) => {
+						setTopLabel(e.target.value);
+						onFocusBottom();
+					}}
+				>
+					{rateList.map((e, i) => (
+						<option
+							key={i + 100}
+							value={e.currency}
+						>{e.currency}</option>
+					))}
+				</select>
+			</div>
+			<div className="row">
+				<input type="number"
+					onChange={(e) => { setBottomValue(e.target.value) }}
+					onFocus={onFocusBottom}
+					onBlur={() => setFocusBottom(false)}
+					value={focusBottom ? bottomValue : countValueBottom}
+				/>
+				<select
+					onChange={(e) => {
+						setBottomLabel(e.target.value);
+						onFocusTop();
+					}}
+					defaultValue={'USD'}
+				>
+					{rateList.map((e, i) => (
+						<option
+							key={i + 1000}
+							value={e.currency}
+						>{e.currency}</option>
+					))}
+				</select>
+			</div>
+		</>
+	)
+}
+
+export default Converter;
--- src/components/exRate/ExRate.js
+++ src/components/exRate/ExRate.js
@@ -1,54 +1,28 @@
-import { useState } from 'react';
-import useSortData from '../../hooks/useSortData';
+import Converter from '../converter/Converter';
 import Spinner from '../spinner/Spinner'
 import './exRate.scss';
 
 const ExRate = (props) => {
-	if (props.data[1]) {
-
-		const rateList = [{
-			currency: "UAH",
-			id: 100000,
-			name: "Українська гривня",
-			rate: 1
-		}, ...useSortData(props.data)]
-
-		return (
-			<main>
-				<h2>Конвертер валют</h2>
-				<p>Конвертація відбувається по середньому банківському курсу Національного банку України</p>
-				<div className="exrate">
-					<div className="row">
-						<input type="text" />
-						<select name="" id="">
-							{rateList.map((e, i) => (<option key={i + 100}>{e.currency}</option>))}
-						</select>
-					</div>
-					<div className="row">
-						<input type="text" />
-						<select name="" id="">
-
-						</select>
-					</div>
-				</div>
-				<h2>
-					Де можна придбати
-				</h2>
-				<ul>
-					<li><a href="https://www.oschadbank.ua/">Ощадбанк</a></li>
-					<li><a href="https://sensebank.com.ua/">Альфа Банк</a></li>
-					<li><a href="https://www.monobank.ua/?lang=uk">Монобанк</a></li>
-					<li><a href="https://privatbank.ua/">Приват Банк</a></li>
-					<li><a href="https://raiffeisen.ua/">Райфайзель Банк</a></li>
-				</ul>
-				<br />
-				<p>Нажми щоб перейти на сайт <br />Курс може відрізнятись в залежності від банку</p>
-			</main>
-		)
-	}
-
 	return (
-		<Spinner />
+		<main>
+			<h2>Конвертер валют</h2>
+			<p>Конвертація відбувається по середньому банківському курсу Національного банку України</p>
+			<div className="exrate">
+				{props.data[1] ? <Converter data={props.data} /> : <Spinner />}
+			</div>
+			<h2>
+				Де можна придбати
+			</h2>
+			<ul>
+				<li><a href="https://www.oschadbank.ua/">Ощадбанк</a></li>
+				<li><a href="https://sensebank.com.ua/">Альфа Банк</a></li>
+				<li><a href="https://www.monobank.ua/?lang=uk">Монобанк</a></li>
+				<li><a href="https://privatbank.ua/">Приват Банк</a></li>
+				<li><a href="https://raiffeisen.ua/">Райфайзель Банк</a></li>
+			</ul>
+			<br />
+			<p>Нажми щоб перейти на сайт <br />Курс може відрізнятись в залежності від банку</p>
+		</main>
 	)
 }
 
